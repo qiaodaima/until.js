820db02ca55f3b111843fed9f684ca0bb9eff78d
liMin <1013880299@qq.com>
2019-10-21 10:57:03 +0800

    修改第一次

--- src/base.js
+++ src/base.js
@@ -112,3 +112,5 @@ export const getElementFeature = (element) => {
  */
 export const $ = (selector) => document.querySelector(selector);
 export const $$ = (selector) => document.querySelectorAll(selector);
+
+//修改第一次
